8d0e387a58f8f762a79f7f1be21100ad1967e613
Ali Mirlou <alimirlou@gmail.com>
2018-07-09 00:47:45 +0430

    Added about and contact urls

--- cssbu_ir/urls.py
+++ cssbu_ir/urls.py
@@ -22,6 +22,10 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 
 	path('', TemplateView.as_view(template_name='index.html'), name='main'),
+
+	path('about', TemplateView.as_view(template_name='about-us.html'), name='about-us'),
+	path('contact', TemplateView.as_view(template_name='contact-us.html'), name='contact-us'),
+
 	path('contests', TemplateView.as_view(template_name='contests.html'), name='contests'),
 	path('gatherings', TemplateView.as_view(template_name='gatherings.html'), name='gatherings'),
 	path('celebrations', TemplateView.as_view(template_name='celebrations.html'), name='celebrations'),
