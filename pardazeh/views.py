eb290f3d0a33016be67dad280353309cbe3051f2
Ali Mirlou <alimirlou@gmail.com>
2018-09-17 20:06:37 +0430

    Fixed ordering of magazines

--- pardazeh/views.py
+++ pardazeh/views.py
@@ -6,3 +6,6 @@ from .models import Magazine
 class AllMagazineView(ListView):
 	model = Magazine
 	template_name = 'pardazeh/pardazeh.html'
+
+	def get_queryset(self):
+		return Magazine.objects.all().order_by('-number')
